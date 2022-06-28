85c107da081368962c718e03dac1393086280b71
kaspoehh <kasperbruin8@gmail.com>
2022-06-28 16:02:47 +0200

    Added new tiles for the overworld

--- Assets/NewWorldGenerator.cs
+++ Assets/NewWorldGenerator.cs
@@ -6,11 +6,13 @@ using UnityEngine.Tilemaps;
 
 public class NewWorldGenerator : MonoBehaviour
 {
+    public static int ChunkSize = 1000;
+    
     public Tilemap tileMap;
     public Tile waterTile;
     public List<Blocks> blocks = new List<Blocks>();
 
-    private Blocks[,] blockIndexes = new Blocks[10, 10];
+    private Blocks[,] blockIndexes = new Blocks[ChunkSize, ChunkSize];
 
     int North = 1;
     int West = 2;
@@ -20,6 +22,7 @@ public class NewWorldGenerator : MonoBehaviour
     private void Start()
     {
         CreateWorld();
+        CheckNeighbours();
         FillWorld();
     }
 
@@ -36,64 +39,47 @@ public class NewWorldGenerator : MonoBehaviour
 
     private void CreateWorld()
     {
-        for (int y = 0; y < 10; y++)
+        for (int y = 0; y < ChunkSize; y++)
         {
-            for (int x = 0; x < 10; x++)
+            for (int x = 0; x < ChunkSize; x++)
             {
-                blockIndexes[x, y] = blocks[16];
+                float sample = Noise.Get2DPerlin(new Vector2(x, y), 0, 1, ChunkSize);
+                
+                if(sample > 0.3)    
+                    blockIndexes[x, y] = blocks[0];
+                else
+                    blockIndexes[x, y] = blocks[16];
             }
         }
     }
 
     private void CheckNeighbours()
     {
-        bool changed = false;
-         for (int y = 1; y < 9; y++)
-         {
-             for (int x = 1; x < 9; x++)
-             {
+        for (int y = 1; y < ChunkSize - 1; y++)
+        {
+            for (int x = 1; x < ChunkSize - 1; x++)
+            {
                 if(!blockIndexes[x,y].isWater)
                 {
                     int blockN = blockIndexes[x, y + 1].blockIndex;                    
                     int blockE = blockIndexes[x + 1, y].blockIndex;
                     int blockS = blockIndexes[x, y - 1].blockIndex;
                     int blockW = blockIndexes[x - 1, y].blockIndex;
-                    
-                    int blockNW = blockIndexes[x - 1, y + 1].blockIndex;
-                    int blockNE = blockIndexes[x + 1, y + 1].blockIndex;
-                    int blockSW = blockIndexes[x - 1, y - 1].blockIndex;
-                    int blockEW = blockIndexes[x - 1, y - 1].blockIndex;
 
                     int number = 1 * blockN + 4 * blockE + 8 * blockS + 2 * blockW;
                     //int number =1 * blockNW + 2 * blockN + 4 * blockNE + 8 * blockW + 16 * blockE + 32 * blockSW + 64 * blockS +
                                 //128 * blockEW;
-                    Debug.Log("xy: " + x + "/" + y + " num:"+  number);
                     blockIndexes[x, y] = blocks[number];
-                    // if(FindBlockByBit(number) != null)
                 }
             }
         }
-        FillWorld();
     }
-
-    Blocks FindBlockByBit(int bit)
-    {
-        foreach (var block in blocks)
-        {
-            if (block.bitmaskValue == bit)
-            {
-                return block;
-            }
-        }
-
-        return null;
-    }
-
+    
     private void FillWorld()
     {
-        for (int y = 0; y < 10; y++)
+        for (int y = 0; y < ChunkSize; y++)
         {
-            for (int x = 0; x < 10; x++)
+            for (int x = 0; x < ChunkSize; x++)
             {
                 Tile blockTile = null;
                 
@@ -105,8 +91,6 @@ public class NewWorldGenerator : MonoBehaviour
                         break;
                     }
                 }
-                
-                
 
                 if (blockIndexes[x, y].isWater)
                 {
